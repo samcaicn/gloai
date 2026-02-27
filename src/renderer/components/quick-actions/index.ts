648a993afc6206b15b428cfae51d0fd5a2971d42
apple <apple@appledeiMac.local>
2026-02-27 20:08:23 +0800

    chore: add 1bak directory to 1bak branch

--- src/renderer/components/quick-actions/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as QuickActionBar } from './QuickActionBar';
-export { default as PromptPanel } from './PromptPanel';
--- src/renderer/constants/app.ts
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = 'GGAi';
-export const APP_ID = 'ggai';
-export const EXPORT_FORMAT_TYPE = 'ggai.providers';
-export const EXPORT_PASSWORD = 'ggai-APP';
